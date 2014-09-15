09a19a90c34c708825b058231a62deafb1ee2f1e
andrezzo <andredanielsson@live.se>
2014-09-15 23:15:15 +0200

    Process klar

--- Process.java
+++ Process.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.io.UnsupportedEncodingException;
 public class Process {
 
     public static void main (String [] args)
@@ -9,46 +11,45 @@ public class Process {
         RandomAccessFile RAFin;
         RandomAccessFile RAFout;
 
+        long startTime = System.currentTimeMillis();
+
         try {
             RAFin = new RandomAccessFile( "/Users/andredanielsson/Documents/adk/ut", "r" );
-            File indexFile = new File( "/Users/andredanielsson/Dropbox/AlgoDataKomp/Lab1/index" );
+            File indexFile = new File( "/Users/andredanielsson/Documents/index" );
 
             // Om det inte finns en fil för index, skapa en ny fil.
             if (indexFile.exists() == false)
                 indexFile.createNewFile();
             RAFout = new RandomAccessFile(indexFile, "rw");
 
-            int[] tempdata;
-            String line = null;
+            int tempdata; // Innehåller den genererade positionen från hashFunction
+            int temppos = 0; // Innehåller den nuvarande positionen i filen "ut"
+            String line = null; // Innehåller en rad text från filen "ut"
+            byte[] compare = null; // Innehåller de senaste en/två/tre bokstavskombinationer
+            Pattern pattern = Pattern.compile(" ");
             
             // Loopa igenom alla rader i ut-filen
             // Tar ut hashat ORD och POSITION samt skriver dessa
             // i indexfilen
-            /* while((line = RAFin.readLine()) != null) {  */
-            for (int i = 0; i < 120000; i++) {
-                line = RAFin.readLine();
-                tempdata = extractData(line);
-                RAFout.seek((long)tempdata[0]);
-                RAFout.writeInt(tempdata[1]);
+            
+            while((line = RAFin.readLine()) != null) {
+
+                String[] split = pattern.split(line);
+
+                // För att undvika att överskriva positioner jämför man
+                // de en/två/tre första bokstäverna
+                byte[] tempCompare = getWord(split[0]);
+
+                if(Arrays.equals(compare, tempCompare) == false) {
+                    compare = tempCompare;
+
+                    tempdata = extractData(line);
+                    RAFout.seek((long)tempdata);
+                    RAFout.writeInt(temppos);
+                }
+                temppos = (int)RAFin.getFilePointer();
             }
 
-            /* int[] data = new int[2]; */
-            /*  */
-            /* data = extractData("testord 278901"); */
-            /* // Tar sig till positionen i filen */
-            /* // X * 4 - 3 */
-            /* RAFout.seek((long)data[0]); */
-            /* System.out.println("pos: " + RAFout.getFilePointer()); */
-            /* RAFout.writeInt(data[1]); */
-            /*  */
-            /* // DEBUG */
-            /* String bla = new String(getWord("aasdasd "), "ISO-8859-1"); */
-            /* System.out.println(bla + "."); */
-            /*  */
-            /* System.out.println("hashfun for aa: " + hashFunction(new String("ab ").getBytes("ISO-8859-1"))); */
-            /*  */
-            /* System.out.println("d - a = " + ('d'-'a' + 1)); */
-            /* System.out.println("e: " + getCharValue('å')); */
 
             RAFin.close();
             RAFout.close();
@@ -56,6 +57,12 @@ public class Process {
         catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        // Tar fram programmets körtid
+        long stopTime = System.currentTimeMillis();
+        long totalTime = stopTime-startTime;
+        System.out.println("Runtime: " + totalTime + " ms, " + (totalTime/1000) + " s.");
 
     }
 
@@ -66,7 +73,7 @@ public class Process {
     private static int hashFunction( byte[] b ){
         if( b[0] == ' ' )
             return 0;
-        
+
         if( b[1] == ' ' )
             return getCharValue(b[0])*900;
 
@@ -84,13 +91,14 @@ public class Process {
      *     ...
      */
     private static int getCharValue( byte b ){
-        int i = new Byte(b).intValue();
+        int i = new Byte(b).intValue(); 
+
         switch(i){
-            case 0xE4:
+            case -28:
                 return 27;
-            case 0xE5:
+            case -27:
                 return 28;
-            case 0xF6:
+            case -10:
                 return 29;
             default:
                 return (1 + b-0x61);
@@ -99,55 +107,62 @@ public class Process {
     
     /*
      * Tar in en string, returnerar en chararray av de en/två/tre första bokstäverna
-     * LETA EFTER EVENTUELLA FEL
      */
     private static byte[] getWord(String s){
-        int len = s.length();
-        byte[] input = s.getBytes();
         byte[] returnArray = new byte[3];
-        switch(len){
-            case 1:
-                returnArray[0] = input[0];
-                returnArray[1] = ' ';
-                returnArray[2] = ' ';
-                break;
-            case 2:
-                returnArray[0] = input[0];
-                returnArray[1] = input[1];
-                returnArray[2] = ' ';
-                break;
-            default:
-                returnArray[0] = input[0];
-                returnArray[1] = input[1];
-                returnArray[2] = input[2];
-                break;
-        }
+        try {
+            int len = s.length();
+            byte[] input = s.getBytes("ISO-8859-1");
 
+            // Skapar ser till att ett visst format skapas
+            switch(len){
+                case 1:
+                    returnArray[0] = input[0];
+                    returnArray[1] = ' ';
+                    returnArray[2] = ' ';
+                    break;
+                case 2:
+                    returnArray[0] = input[0];
+                    returnArray[1] = input[1];
+                    returnArray[2] = ' ';
+                    break;
+                default:
+                    returnArray[0] = input[0];
+                    returnArray[1] = input[1];
+                    returnArray[2] = input[2];
+                    break;
+            }
+
+        }
+        catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        
         return returnArray;
+
     }
 
     /*
      * Tar in en string s med format "ORD POSITION"
-     * och returnerar
-     * [0]: hashvärde
-     * [1]: position i texten
+     * och returnerar hashvärde
      */
-    private static int[] extractData(String s){
-        int[] returnArray = new int[2];
+    private static int extractData(String s){
+        int returnValue = 0;
         Pattern pattern = Pattern.compile(" ");
         String[] split = pattern.split(s); // [0] - Ordet, [1] positionen
 
         // Ta ut de en/två/tre första bokstäverna
-        byte[] word = getWord(new StringBuilder(split[0]).append(' ').toString());
+        byte[] word = getWord(split[0]);
 
-        // positionen konverteras från string till int
-        returnArray[1] = Integer.parseInt(split[1]);
+        // Får ut hashfunktionens värde baserat på de en/två/tre första bokstäverna
+        returnValue = hashFunction(word);
 
-        // Avstånd 4 bytes mellan int
-        if (returnArray[0] > 0)
-            returnArray[0] = returnArray[0] * 4 - 3;
+        // Skapar ett avstånd 4 bytes så att inget skrivs över
+        // (då vi skriver med int)
+        if (returnValue > 0)
+            returnValue = returnValue * 4 - 3;
 
-        return returnArray;
+        return returnValue;
     }
 
 }
